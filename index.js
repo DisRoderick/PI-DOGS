02d4a023b823f5064128b2d13e88f00edcc86230
DisRoderick <roderickrodriguez706@gmail.com>
2023-04-06 13:07:07 -0500

    back end deploy finalizado

--- index.js
+++ index.js
@@ -19,10 +19,15 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
+const {
+  PORT
+} = process.env;
+const port = PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
--- src/db.js
+++ src/db.js
@@ -6,16 +6,6 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-
-
-
-
-
-
-
-
-
-
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
